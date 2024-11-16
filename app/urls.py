ec2206581326375b679c08f35a5dfb658f25b45d
bn-merwane <m.bensalah@esi-sba.dz>
2024-11-16 21:47:58 +0100

    .

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 from app.views import test1
 
 urlpatterns = [
-  path(('test/',test1))
+    path('test/', test1),  # Corrected this line by removing the extra parentheses
 ]
