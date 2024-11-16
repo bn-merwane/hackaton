a985aeac3d084413f12f96846582d36b03b5ac05
bn-merwane <m.bensalah@esi-sba.dz>
2024-11-16 21:53:35 +0100

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,8 +1,8 @@
 # views.py
 from django.http import JsonResponse
-from .models import Text
+from .models import test
 
 def test1(request):
     # Assuming you want to return a specific Text object
-    text_object = Text.objects.get(id=1)  # Fetch the Text object with ID 1
+    text_object = test.objects.get(id=1)  # Fetch the Text object with ID 1
     return JsonResponse({'content': text_object.text})
